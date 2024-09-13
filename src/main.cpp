b6716193a08e4b3b1d1e9c7de4d3979c7af464c6
6haitandimon <borejko2020@gmail.com>
2024-09-13 23:04:02 +0300

    Add and update lib Delete Magnitometr read data from ICM20948 (comment) Add i2c read and write semaphor Update init function Add Telemetry and telemetry lib Add new task "print_terminal" (debug from ground) and "icm_task" Setting stask size

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,14 @@
 #include <cstdio>
 
 #include "m100Mini.h"
+#include "BME280.h"
+#include "ICM20948.h"
+#include "Telemetry.h"
+
+Telemetry telemetry;
 
 
+ICM::ICM20948 icm20948 = ICM::ICM20948(i2c0, 4, 5, 0x68);
 GPS::M100Mini m100Mini = GPS::M100Mini(uart1, 8, 9, 115200);
 
 void led_task(void*)
@@ -21,26 +27,48 @@ void led_task(void*)
     }
 }
 
-void gps_task(void*){
-  GPS::NAV_POSLLH navData;
+void print_terminal(void*){
   while(true){
-    if(m100Mini.GetGPS(&navData)){
-      printf("Lat: %ld, Lon: %ld\n", navData.lat, navData.lon);
-    }else{
-      printf("wait gps\n");
-    }
+    printf("GPS\nLat: %ld, Lon: %ld\n\n", telemetry.gpsData.lat, telemetry.gpsData.lon);
+    printf("ICM Accel Data\n x: %f, y: %f, z:%f\n\n", telemetry.icm20948Data.Accsel_x, telemetry.icm20948Data.Accsel_y, telemetry.icm20948Data.Accsel_z);
     vTaskDelay(50 / portTICK_PERIOD_MS);
   }
 }
+
+void gps_task(void*){
+  while(true){
+    if(m100Mini.GetGPS(&telemetry.gpsData));
+
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
+}
+
+void icm_task(void*){
+  while (true) {
+    icm20948.getAllData(&telemetry.icm20948Data);
+
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
+}
+
 
 int main()
 {
     stdio_init_all();
 
-    m100Mini.m100Mini_Init();
+    gpio_init(2);
 
-    xTaskCreate(gps_task, "GPS_task", 256, NULL, 1, NULL);
-    xTaskCreate(led_task, "LED_task", 256, NULL, 2, NULL);
+    gpio_put(2, false);
+    gpio_put(2, true);
+
+    m100Mini.m100Mini_Init();
+    icm20948.init();
+
+    xTaskCreate(gps_task, "GPS_task", 256, NULL, 2, NULL);
+    xTaskCreate(icm_task, "ICM_task", 256, NULL, 2, NULL);
+    xTaskCreate(led_task, "LED_task", 256, NULL, 1, NULL);
+    xTaskCreate(print_terminal, "TerminalPC", 256, NULL, 1, NULL);
+
     vTaskStartScheduler();
 
     while(true){};
