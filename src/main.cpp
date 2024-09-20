eb819854e041f04d83313cf98f9b2b22f6913575
6haitandimon <borejko2020@gmail.com>
2024-09-20 22:45:47 +0300

    Add lora sx1276 support busy_wait_x -> vTaskDelay add optimization and speed add lora task TODO: add queue tasks and Calman filtr

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,18 @@
 #include "m100Mini.h"
 #include "BME280.h"
 #include "ICM20948.h"
+#include "loraPaket.h"
+#include "sx1276.h"
 #include "Telemetry.h"
+
 
 Telemetry telemetry;
 
 
 ICM::ICM20948 icm20948 = ICM::ICM20948(i2c0, 4, 5, 0x68);
 GPS::M100Mini m100Mini = GPS::M100Mini(uart1, 8, 9, 115200);
+LoRa lora = LoRa(spi0, 16, 19, 18, 17, 20, 21);
+
 
 void led_task(void*)
 {
@@ -51,9 +56,22 @@ void icm_task(void*){
   }
 }
 
+void lora_task(void*){
+  while(true){
+    LoRaPacket packet = LoRaPacket(reinterpret_cast<unsigned char *>(&telemetry), sizeof(struct Telemetry));
+
+    lora.transmitPacket(&packet);
+
+    printf("lora send!\n");
+
+    vTaskDelay(12 / portTICK_PERIOD_MS);
+  }
+}
+
 
 int main()
 {
+
     stdio_init_all();
 
     gpio_init(2);
@@ -63,11 +81,22 @@ int main()
 
     m100Mini.m100Mini_Init();
     icm20948.init();
+    if(lora.begin()){
+      lora.setFrequency(915000000)
+          ->setTXPower(10)
+          ->setBandwidth(LoRa::BW_500k)
+          ->setSpreadFactor(LoRa::SF_10)
+          ->setCodingRate(LoRa::CR_45)
+          ->setSyncWord(0x12)
+          ->setupRXIrq();
+      //    flags.flagUpdate = true;
+    }
 
     xTaskCreate(gps_task, "GPS_task", 256, NULL, 2, NULL);
     xTaskCreate(icm_task, "ICM_task", 256, NULL, 2, NULL);
     xTaskCreate(led_task, "LED_task", 256, NULL, 1, NULL);
-    xTaskCreate(print_terminal, "TerminalPC", 256, NULL, 1, NULL);
+//    xTaskCreate(print_terminal, "TerminalPC", 256, NULL, 1, NULL);
+    xTaskCreate(lora_task, "LoRa_task", 256, NULL, 3, NULL);
 
     vTaskStartScheduler();
 
